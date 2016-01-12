b6a5a5eeb09ad69539fade9ce88dadf2014a9d4b
Jarrett Revels <jarrettrevels@gmail.com>
2016-01-12 13:59:21 -0500

    remove extraneous parameterization from problem-based benchmarks

--- src/problem/ProblemBenchmarks.jl
+++ src/problem/ProblemBenchmarks.jl
@@ -22,7 +22,7 @@ include("IMDBGraphs.jl")
 
 @track BaseBenchmarks.TRACKER begin
     @benchmarks begin
-        [(:imdb_centrality, n) => IMDBGraphs.perf_imdb_centrality(n) for n in (5, 50)]
+        (:imdb_centrality,) => IMDBGraphs.perf_imdb_centrality(50)
     end
     @tags "problem" "example" "kernel" "graph" "centrality" "imdb"
 end
@@ -34,10 +34,10 @@ end
 include("MonteCarlo.jl")
 
 @track BaseBenchmarks.TRACKER begin
-    @setup npaths = (10, 10^2, 10^3, 10^4)
+    @setup n = 10^4
     @benchmarks begin
-        [(:euro_option_devec, n) => MonteCarlo.perf_euro_option_devec(n) for n in npaths]
-        [(:euro_option_vec, n) => MonteCarlo.perf_euro_option_vec(n) for n in npaths]
+        (:euro_option_devec,) => MonteCarlo.perf_euro_option_devec(n)
+        (:euro_option_vec,) => MonteCarlo.perf_euro_option_vec(n)
     end
     @tags "problem" "example" "kernel" "monte carlo" "finance" "vectorization" "random" "inplace"
 end
@@ -50,14 +50,14 @@ include("Laplacian.jl")
 
 @track BaseBenchmarks.TRACKER begin
     @setup begin
-        sparse_sizes = (8^4, 8^5)
-        iter_sizes = (8, 8^2)
+        sparse_size = 8^5
+        iter_size = 8^2
     end
     @benchmarks begin
-        [(:laplace_sparse_matvec, n) => Laplacian.perf_laplace_sparse_matvec(n) for n in sparse_sizes]
-        [(:laplace_iter_devec, n) => Laplacian.perf_laplace_iter_devec(n) for n in iter_sizes]
-        [(:laplace_iter_vec, n) => Laplacian.perf_laplace_iter_vec(n) for n in iter_sizes]
-        [(:laplace_iter_sub, n) => Laplacian.perf_laplace_iter_devec(n) for n in iter_sizes]
+        (:laplace_sparse_matvec,) => Laplacian.perf_laplace_sparse_matvec(sparse_size)
+        (:laplace_iter_devec,) => Laplacian.perf_laplace_iter_devec(iter_size)
+        (:laplace_iter_vec,) => Laplacian.perf_laplace_iter_vec(iter_size)
+        (:laplace_iter_sub,) => Laplacian.perf_laplace_iter_devec(iter_size)
     end
     @tags "problem" "example" "kernel" "laplacian" "iterative" "sparse" "vectorization" "subarray" "linalg" "array"
 end
@@ -70,7 +70,7 @@ include("GrigoriadisKhachiyan.jl")
 
 @track BaseBenchmarks.TRACKER begin
     @benchmarks begin
-        [(:grigoriadis_khachiyan, n) => GrigoriadisKhachiyan.perf_gk(n, [0.1]) for n in (10, 10^2, 10^3)]
+        (:grigoriadis_khachiyan,) => GrigoriadisKhachiyan.perf_gk(350, [0.1])
     end
     @tags "problem" "example" "kernel" "grigoriadis" "khachiyan" "game"
 end
@@ -83,7 +83,7 @@ include("GoGame.jl")
 
 @track BaseBenchmarks.TRACKER begin
     @benchmarks begin
-        [(:go_game, n) => GoGame.perf_go_game(n) for n in (10, 20)]
+        (:go_game,) => GoGame.perf_go_game(10)
     end
     @tags "problem" "example" "kernel" "go" "game"
 end
