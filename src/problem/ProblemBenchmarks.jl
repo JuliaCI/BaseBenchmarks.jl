0eeed98f3a88ba4b032d8c773f4705cfcc4fc81d
Jarrett Revels <jarrettrevels@gmail.com>
2016-01-25 11:28:02 -0500

    fix typo (called the wrong function)

--- src/problem/ProblemBenchmarks.jl
+++ src/problem/ProblemBenchmarks.jl
@@ -56,7 +56,7 @@ include("Laplacian.jl")
         (:laplace_sparse_matvec,) => Laplacian.perf_laplace_sparse_matvec(sparse_size)
         (:laplace_iter_devec,) => Laplacian.perf_laplace_iter_devec(iter_size)
         (:laplace_iter_vec,) => Laplacian.perf_laplace_iter_vec(iter_size)
-        (:laplace_iter_sub,) => Laplacian.perf_laplace_iter_devec(iter_size)
+        (:laplace_iter_sub,) => Laplacian.perf_laplace_iter_sub(iter_size)
     end
     @tags "problem" "example" "kernel" "laplacian" "iterative" "sparse" "vectorization" "subarray" "linalg" "array"
 end
