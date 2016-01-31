ddaac6917ab05daf18f40fe7d4b895ea9ab9ae1a
Katie Hyatt <kshyatt@physics.ucsb.edu>
2016-01-30 18:21:50 -0800

    More benchmarks for eig on special types

--- src/lapack/LAPACKBenchmarks.jl
+++ src/lapack/LAPACKBenchmarks.jl
@@ -17,13 +17,33 @@ const SIZES = (2^2, 2^4, 2^6, 2^8, 2^10)
             A = A + im*BaseBenchmarks.samerand(size(A)...)
             return A + A'
         end
+        tridiag_arrays = map(SIZES) do n
+            d_ = BaseBenchmarks.samerand(n)
+            e_ = BaseBenchmarks.samerand(n-1)
+            f_ = BaseBenchmarks.samerand(n-1)
+            return Tridiagonal(f_,d_,e_)
+        end
+        symtri_arrays = map(SIZES) do n
+            d_ = BaseBenchmarks.samerand(n)
+            e_ = BaseBenchmarks.samerand(n-1)
+            return SymTridiagonal(d_,e_)
+        end
+        uptri_arrays = map( A -> UpperTriangular(A), real_arrays)
+        lotri_arrays = map( A -> LowerTriangular(A), real_arrays)
+        unituptri_arrays = map( A -> Base.LinAlg.UnitUpperTriangular(A), real_arrays)
+        unitlotri_arrays = map( A -> Base.LinAlg.UnitLowerTriangular(A), real_arrays)
     end
     @benchmarks begin
         [(:realeig, size(A, 1)) => eig(A) for A in real_arrays]
         [(:symeig, size(A, 1)) => eig(A) for A in sym_arrays]
         [(:hermeig, size(A, 1)) => eig(A) for A in herm_arrays]
+        [(:symtrieig, size(A, 1)) => eigfact(A) for A in symtri_arrays]
+        [(:uptrieig, size(A, 1)) => eigfact(A) for A in uptri_arrays]
+        [(:lotrieig, size(A, 1)) => eigfact(A) for A in lotri_arrays]
+        [(:unituptrieig, size(A, 1)) => eigfact(A) for A in unituptri_arrays]
+        [(:unitlotrieig, size(A, 1)) => eigfact(A) for A in unitlotri_arrays]
     end
-    @tags "array" "lapack" "linalg" "eig"
+    @tags "array" "lapack" "linalg" "eig" "eigfact" "factorization"
 end
 
 #################
