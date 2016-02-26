e57d4026905e6cc0b11e9524bf52412d8f93c493
Jarrett Revels <jarrettrevels@gmail.com>
2016-02-25 21:56:42 -0500

    update runtrials.jl timings

--- bin/runtrials.jl
+++ bin/runtrials.jl
@@ -9,16 +9,17 @@ trialslog = open("trials.log", "w")
 using BaseBenchmarks
 using JLD
 
-const times = (5.0,)
+const times = (5.0, 5.0)
 const group = ENSEMBLE["factorization eig"]
 const trials = 50
 
 println(trialslog, now(), " | WARMING UP BENCHMARKS..."); flush(trialslog)
 ntrials(group, 1, 1e-6; verbose = true)
 
-for t in times
+for i in 1:length(times)
+    t = times[i]
     println(trialslog, now(), " | RUNNING $(trials) TRIALS AT T = $(t)..."); flush(trialslog)
-    jldopen("trials$(Int(t)).jld", "w") do file
+    jldopen("trials_$(Int(t))_$(i).jld", "w") do file
         write(file, "trials", ntrials(group, trials, t; verbose = true))
     end
 end
