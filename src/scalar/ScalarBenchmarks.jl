f93f28a225fde3fa99985918b87668ad01efc245
Jarrett Revels <jarrettrevels@gmail.com>
2016-03-01 18:22:21 -0500

    add tuning parameters to scalar benchmarks

--- src/scalar/ScalarBenchmarks.jl
+++ src/scalar/ScalarBenchmarks.jl
@@ -9,29 +9,32 @@ const REALS = (INTS..., FLOATS...)
 const COMPS = map(R -> Complex{R}, REALS)
 const NUMS = (REALS..., COMPS...)
 
+const SCALAR_TIME = 0.1
+const SCALAR_GC = true
+
 ##############
 # predicates #
 ##############
 
 g = addgroup!(GROUPS, "scalar predicate",  ["scalar", "predicate", "isinteger", "isinf",
-                                              "isnan", "iseven", "isodd"])
+                                            "isnan", "iseven", "isodd"])
 
 for T in NUMS
     x = one(T)
     tstr = string(T)
-    g["isequal", tstr]   = @benchmarkable isequal($x, $x)
-    g["isless", tstr]    = @benchmarkable isequal($x, $x)
-    g["isinteger", tstr] = @benchmarkable isinteger($x)
-    g["isinf", tstr]     = @benchmarkable isinf($x)
-    g["isfinite", tstr]  = @benchmarkable isfinite($x)
-    g["isnan", tstr]     = @benchmarkable isnan($x)
+    g["isequal", tstr]   = @benchmarkable(isequal($x, $x), $SCALAR_TIME)
+    g["isless", tstr]    = @benchmarkable(isequal($x, $x), $SCALAR_TIME)
+    g["isinteger", tstr] = @benchmarkable(isinteger($x), $SCALAR_TIME)
+    g["isinf", tstr]     = @benchmarkable(isinf($x), $SCALAR_TIME)
+    g["isfinite", tstr]  = @benchmarkable(isfinite($x), $SCALAR_TIME)
+    g["isnan", tstr]     = @benchmarkable(isnan($x), $SCALAR_TIME)
 end
 
 for T in INTS
     x = one(T)
     tstr = string(T)
-    g["iseven", tstr] = @benchmarkable iseven($x)
-    g["isodd", tstr] = @benchmarkable isodd($x)
+    g["iseven", tstr] = @benchmarkable(iseven($x), $SCALAR_TIME, $SCALAR_GC)
+    g["isodd", tstr] = @benchmarkable(isodd($x), $SCALAR_TIME, $SCALAR_GC)
 end
 
 ##############
@@ -48,14 +51,14 @@ g = addgroup!(GROUPS, "scalar arithmetic",  ["scalar", "arithmetic", "fastmath"]
 for Ti in NUMS, Tj in NUMS
     xi, xj = one(Ti), one(Tj)
     tistr, tjstr = string(Ti), string(Tj)
-    g["scalar_add", tistr, tjstr] = @benchmarkable +($xi, $xj)
-    g["scalar_sub", tistr, tjstr] = @benchmarkable -($xi, $xj)
-    g["scalar_mul", tistr, tjstr] = @benchmarkable *($xi, $xj)
-    g["scalar_div", tistr, tjstr] = @benchmarkable /($xi, $xj)
-    g["scalar_fastmath_add", tistr, tjstr] = @benchmarkable perf_fastmath_add($xi, $xj)
-    g["scalar_fastmath_sub", tistr, tjstr] = @benchmarkable perf_fastmath_sub($xi, $xj)
-    g["scalar_fastmath_mul", tistr, tjstr] = @benchmarkable perf_fastmath_mul($xi, $xj)
-    g["scalar_fastmath_div", tistr, tjstr] = @benchmarkable perf_fastmath_div($xi, $xj)
+    g["scalar_add", tistr, tjstr] = @benchmarkable(+($xi, $xj), $SCALAR_TIME, $SCALAR_GC)
+    g["scalar_sub", tistr, tjstr] = @benchmarkable(-($xi, $xj), $SCALAR_TIME, $SCALAR_GC)
+    g["scalar_mul", tistr, tjstr] = @benchmarkable(*($xi, $xj), $SCALAR_TIME, $SCALAR_GC)
+    g["scalar_div", tistr, tjstr] = @benchmarkable(/($xi, $xj), $SCALAR_TIME, $SCALAR_GC)
+    g["scalar_fastmath_add", tistr, tjstr] = @benchmarkable(perf_fastmath_add($xi, $xj), $SCALAR_TIME, $SCALAR_GC)
+    g["scalar_fastmath_sub", tistr, tjstr] = @benchmarkable(perf_fastmath_sub($xi, $xj), $SCALAR_TIME, $SCALAR_GC)
+    g["scalar_fastmath_mul", tistr, tjstr] = @benchmarkable(perf_fastmath_mul($xi, $xj), $SCALAR_TIME, $SCALAR_GC)
+    g["scalar_fastmath_div", tistr, tjstr] = @benchmarkable(perf_fastmath_div($xi, $xj), $SCALAR_TIME, $SCALAR_GC)
 end
 
 end # module
