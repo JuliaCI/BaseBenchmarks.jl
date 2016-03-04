8001ade4c9393d213a8aeee94d13f2873817c626
Jarrett Revels <jarrettrevels@gmail.com>
2016-03-04 10:04:52 -0500

    guarantee at least 64-byte alignment on arrays generated by samerand

--- src/utils/RandUtils.jl
+++ src/utils/RandUtils.jl
@@ -1,8 +1,17 @@
 module RandUtils
 
 const SEED = MersenneTwister(1)
+const ALIGN = 64
 
-samerand(args...) = rand(deepcopy(SEED), args...)
+# ensures the generated array always has the same alignment (at least ALIGN-byte aligned)
+function samerand(args...)
+    v = rand(deepcopy(SEED), args...)
+    if Int(pointer(v)) % ALIGN == 0
+       return v
+    else
+       return samerand(args...)
+    end
+end
 
 randvec(T, n) = samerand(T, n)
 randvec(n) = samerand(n)
