53f38529f2c15a30fa8262cf385ede5ad64b171c
Jarrett Revels <jarrettrevels@gmail.com>
2016-04-14 11:12:23 -0400

    add code to retune specific benchmarks instead of the entire suite

--- etc/tune.jl
+++ etc/tune.jl
@@ -4,6 +4,13 @@ using JLD
 using BaseBenchmarks
 using BenchmarkTools
 
-@warmup BaseBenchmarks.SUITE
-tune!(BaseBenchmarks.SUITE; seconds = 10, verbose = true)
-JLD.save("evals.jld", "SUITE", evals(BaseBenchmarks.SUITE))
+# re-tune the entire suite
+# @warmup BaseBenchmarks.SUITE
+# tune!(BaseBenchmarks.SUITE; seconds = 10, verbose = true)
+# JLD.save("evals.jld", "SUITE", evals(BaseBenchmarks.SUITE))
+
+# re-tune a specific group/benchmark
+# result = tune!(BaseBenchmarks.SUITE[g][b]; seconds = 10)
+# evalsgroup = JLD.load("evals.jld", "SUITE")
+# evalsgroup[g][b] = evals(result)
+# JLD.save("evals.jld", "SUITE", evalsgroup)
