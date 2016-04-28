4a9cb25def533b67014a8c3cf679034363c982ad
Jarrett Revels <jarrettrevels@gmail.com>
2016-04-28 17:38:02 -0400

    page aligning arrays is overkill

--- src/utils/RandUtils.jl
+++ src/utils/RandUtils.jl
@@ -3,20 +3,7 @@ module RandUtils
 const SEED = MersenneTwister(1)
 const DEFAULT_ELTYPE = typeof(rand())
 
-# ensures the generated array is always aligned to the page size
-function alignedalloc{N}(S, dims::NTuple{N,Int})
-    randarr = rand(deepcopy(SEED), S, dims...)
-    alignedarr = Mmap.mmap(Array{eltype(S),N}, dims)
-    copy!(alignedarr, randarr)
-    @assert Int(pointer(alignedarr)) % 64 == 0 # sanity check
-    return alignedarr
-end
-
-samerand() = rand(deepcopy(SEED))
-samerand(S) = rand(deepcopy(SEED), S)
-samerand(dims::Int...) = alignedalloc(DEFAULT_ELTYPE, dims)
-samerand(S, dims::Int...) = alignedalloc(S, dims)
-
+samerand(args...) = rand(deepcopy(SEED), args...)
 samesprand(args...) = sprand(deepcopy(SEED), args...)
 samesprandbool(args...) = sprandbool(deepcopy(SEED), args...)
 
