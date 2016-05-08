6dd9b052f3928cbde8769208d9189999418a8231
Jarrett Revels <jarrettrevels@gmail.com>
2016-05-07 20:15:27 -0400

    compat fix for sprandbool

--- src/utils/RandUtils.jl
+++ src/utils/RandUtils.jl
@@ -5,7 +5,12 @@ const DEFAULT_ELTYPE = typeof(rand())
 
 samerand(args...) = rand(deepcopy(SEED), args...)
 samesprand(args...) = sprand(deepcopy(SEED), args...)
-samesprandbool(args...) = sprandbool(deepcopy(SEED), args...)
+
+if VERSION >= v"0.5.0-dev+3807"
+    samesprandbool(args...) = sprand(deepcopy(SEED), Bool, args...)
+else
+    samesprandbool(args...) = sprandbool(deepcopy(SEED), args...)
+end
 
 randvec(T, n) = samerand(T, n)
 randvec(n) = samerand(n)
