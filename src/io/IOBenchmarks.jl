6a45a2fdf5014431f0f1c2634be7aca243402472
Steven G. Johnson <stevenj@mit.edu>
2016-10-04 15:51:16 -0400

    serialization benchmarks

--- src/io/IOBenchmarks.jl
+++ src/io/IOBenchmarks.jl
@@ -30,4 +30,28 @@ testbuf = IOBuffer(randstring(10^4))
 g["read"]       = @benchmarkable perf_read!($testbuf)
 g["readstring"] = @benchmarkable readstring($testbuf)
 
+#################################
+# serialization (#18633, #7893) #
+#################################
+
+g = addgroup!(SUITE, "serialization", ["buffer", "string"])
+
+function serialized_buf(x)
+    io = IOBuffer()
+    serialize(io, x)
+    return io
+end
+
+teststrings = [randstring(32) for i=1:10^3]
+teststrings_buf = serialized_buf(teststrings)
+
+g["serialize", "Vector{String}"] = @benchmarkable serialize(io, $teststrings) setup=(io=IOBuffer())
+g["deserialize", "Vector{String}"] = @benchmarkable deserialize($teststrings_buf) setup=(seek($teststrings_buf, 0))
+
+testdata = rand(1000,1000)
+testdata_buf = serialized_buf(testdata)
+
+g["serialize", "Matrix{Float64}"] = @benchmarkable serialize(io, $testdata) setup=(io=IOBuffer())
+g["deserialize", "Matrix{Float64}"] = @benchmarkable deserialize($testdata_buf) setup=(seek($testdata_buf, 0))
+
 end # module
