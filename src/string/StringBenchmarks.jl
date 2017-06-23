ad3e5d7b37908583f62553afdc05eff6bc0b9f7c
Mus M <mus-m@outlook.com>
2017-06-22 20:18:28 -0400

    Add search and searchindex string benchmarks (#80)

--- src/string/StringBenchmarks.jl
+++ src/string/StringBenchmarks.jl
@@ -19,4 +19,14 @@ str = join(samerand('a':'d', 10^4))
 SUITE["replace"] = @benchmarkable replace($str, "a", "b")
 SUITE["join"] = @benchmarkable join($str, $str) time_tolerance=0.40
 
+str = "Gf6FPPWevqer3di13haDSzrRrSiThqmV3k02dALLu7OHdYRR0dfrKf4iCMcDvgZBawx"
+
+g = addgroup!(SUITE, "search")
+g["Char"] = @benchmarkable search($str,  $('x'))
+g["String"] = @benchmarkable search($str, $("x"))
+
+g = addgroup!(SUITE, "searchindex")
+g["Char"] = @benchmarkable searchindex($str,  $('x'))
+g["String"] = @benchmarkable searchindex($str, $("x"))
+
 end # module
