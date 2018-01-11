2dedf87aeaccaae3b245fd84b2b1a2cc850c042e
Tim Holy <tim.holy@gmail.com>
2018-01-11 08:42:19 -0600

    Void->Nothing deprecation fix

--- src/union/UnionBenchmarks.jl
+++ src/union/UnionBenchmarks.jl
@@ -9,7 +9,7 @@ using Compat
 const SUITE = BenchmarkGroup()
 
 ########################
-# array Union{T, Void} #
+# array Union{T, Nothing} #
 ########################
 
 g = addgroup!(SUITE, "array")
@@ -17,7 +17,7 @@ g = addgroup!(SUITE, "array")
 const VEC_LENGTH = 1000
 
 _zero(::Type{T}) where {T} = zero(T)
-_zero(::Type{Union{T, Void}}) where {T} = zero(T)
+_zero(::Type{Union{T, Nothing}}) where {T} = zero(T)
 
 function perf_sum(X::AbstractArray{T}) where T
     s = _zero(T) + _zero(T)
@@ -53,10 +53,10 @@ for T in (Bool, Int8, Int64, Float32, Float64, BigInt, BigFloat, Complex{Float64
     end
 
     # 10% of missing values
-    X = Vector{Union{T, Void}}(Vector{T}(samerand(S, VEC_LENGTH)))
-    Y = Vector{Union{T, Void}}(Vector{T}(samerand(S, VEC_LENGTH)))
-    X2 = Vector{Union{T, Void}}(Vector{T}(samerand(S, VEC_LENGTH)))
-    Y2 = Vector{Union{T, Void}}(Vector{T}(samerand(S, VEC_LENGTH)))
+    X = Vector{Union{T, Nothing}}(Vector{T}(samerand(S, VEC_LENGTH)))
+    Y = Vector{Union{T, Nothing}}(Vector{T}(samerand(S, VEC_LENGTH)))
+    X2 = Vector{Union{T, Nothing}}(Vector{T}(samerand(S, VEC_LENGTH)))
+    Y2 = Vector{Union{T, Nothing}}(Vector{T}(samerand(S, VEC_LENGTH)))
     X2[samerand(VEC_LENGTH) .> .9] = nothing
     Y2[samerand(VEC_LENGTH) .> .9] = nothing
 
