ff1f2a0d7a86d0593f2d916c64f52fe71328a5a4
Andy Ferris <andy.ferris@roames.com.au>
2018-06-29 16:45:43 +1000

    Adapt to new `init` keyword for `reduce` family of functions

--- src/array/sumindex.jl
+++ src/array/sumindex.jl
@@ -201,11 +201,20 @@ function setup_mapr_access(A)
     B = Vector{typeof(zz)}(undef, n)
     B, zz, n
 end
-function perf_mapr_access(A, B, zz, n) #20517
-    @inbounds for j in 1:n
-        B[j] = mapreduce(k -> A[j,k]*A[k,j], +, zz, 1:j)
+if VERSION < v"0.7.0-beta-81"
+    function perf_mapr_access(A, B, zz, n) #20517
+        @inbounds for j in 1:n
+            B[j] = mapreduce(k -> A[j,k]*A[k,j], +, zz, 1:j)
+        end
+        B
     end
-    B
+else
+    function perf_mapr_access(A, B, zz, n) #20517
+        @inbounds for j in 1:n
+            B[j] = mapreduce(k -> A[j,k]*A[k,j], +, 1:j; init=zz)
+        end
+        B
+    end
 end
 
 ##########################
