bcec2143f4f956790b40d12f71dcfa76d15a5460
Pete Vilter <7341+vilterp@users.noreply.github.com>
2022-01-26 10:56:48 -0500

    add simple allocation benchmarks (#291)

---
+++ src/alloc/AllocBenchmarks.jl
@@ -0,0 +1,60 @@
+module AllocBenchmarks
+
+using BenchmarkTools
+
+const SUITE = BenchmarkGroup()
+
+function perf_alloc_many_arrays()
+    for _ in 1:100
+        # global to ensure that this is heap allocated
+        global a = [[] for _ in 1:1000]
+
+        GC.safepoint()
+        Threads.atomic_fence()
+    end
+end
+
+function perf_alloc_many_strings()
+    for i in 1:100
+        # global to ensure that this is heap allocated
+        global b = ["hello $(j)" for j in 1:1000]
+
+        GC.safepoint()
+        Threads.atomic_fence()
+    end
+end
+
+# mutable to make it heap allocate
+mutable struct Foo
+    x::Int
+    y::Int
+end
+
+function perf_alloc_many_structs()
+    for i in 1:100
+        # global to ensure that this is heap allocated
+        global b = [Foo(i, j) for j in 1:1000]
+
+        GC.safepoint()
+        Threads.atomic_fence()
+    end
+end
+
+function perf_grow_array()
+    global x = Vector{Int}()
+    for i in 1:100
+        for j in 1:1000
+            push!(x, j)
+        end
+
+        GC.safepoint()
+        Threads.atomic_fence()
+    end
+end
+
+SUITE["arrays"] = @benchmarkable perf_alloc_many_arrays()
+SUITE["strings"] = @benchmarkable perf_alloc_many_strings()
+SUITE["structs"] = @benchmarkable perf_alloc_many_structs()
+SUITE["grow_array"] = @benchmarkable perf_grow_array()
+
+end # module
