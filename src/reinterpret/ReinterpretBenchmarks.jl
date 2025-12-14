b6600e147a753dc6e3f4dda7c45a6095cb81caf1
Nathan Daly <nathan.daly@relational.ai>
2025-12-13 21:57:42 -0700

    Decent set of reinterpret benchmarks

--- src/reinterpret/ReinterpretBenchmarks.jl
+++ src/reinterpret/ReinterpretBenchmarks.jl
@@ -23,39 +23,72 @@ g = addgroup!(SUITE, "packed_types")
 
 # Define extra primitive types for benchmarking
 primitive type Int24 3 * 8 end
-primitive type Int40 5 * 8 end
-primitive type Int48 6 * 8 end
-primitive type Int56 7 * 8 end
 
 primitive type Int136 17 * 8 end
-primitive type Int144 18 * 8 end
 
 primitive type Int384 48 * 8 end
 primitive type Int392 49 * 8 end
-primitive type Int400 50 * 8 end
 
 primitive type Int1024 128 * 8 end
 
-for B in (3, 4, 6, 7, 17, 18, 48, 49, 50, 128)
+for B in (1, 2, 3, 8, 16, 17, 48, 49, 128)
     let T = eval(Symbol("Int$(B*8)"))
-        g[B] = @benchmarkable bench_reinterpret($T, $(ntuple(i->UInt8(i), B)), 1000)
+        g[B] = @benchmarkable bench_reinterpret($T, $(ntuple(i->UInt8(i), B)))
     end
 end
 
-function perf_reinterpret_()
-    TUPLES = [
+g = addgroup!(SUITE, "padded_types")
+
+primitive type Int80 10 * 8 end
+primitive type Int232 29 * 8 end
+primitive type Int800 100 * 8 end
+primitive type Int832 104 * 8 end
+primitive type Int936 117 * 8 end
+primitive type Int1824 228 * 8 end
+
+for tup in [
         (0x01, 1, 0x02),
-        (1.0, 2, 3.0, 4, 5.0, 6, 7.0, 8, 9.0, 10, 11.10, 12, 13.0),
         (1, 0x0001, (0x01, 2, 0x01), 0x01, 1.0),
-        ntuple(i->0x01, 100),
-        ntuple(i->0x01, 228),
         (0x01, 1, 2, ntuple(i->0x01, 100),),
     ]
-    for tup in TUPLES
-        bench_normalization(tup)
-    end
+    B1 = sizeof(tup)
+    B2 = Base.packedsize(typeof(tup))
+    T2 = eval(Symbol("Int$(B2*8)"))
+    g[B2, B1] = @benchmarkable bench_reinterpret($T2, $(tup))
 end
 
-# SUITE["arrays"] = @benchmarkable perf_alloc_many_arrays()
+g = addgroup!(SUITE, "mixed_tuples")
+
+for tup in [
+        (1.0, 2, 3.0, 4, 5.0, 6, 7.0, 8, 9.0, 10, 11.10, 12, 13.0),
+        ntuple(i->(isodd(i) ? Int32(i) : Float32(i)), 25),
+        ntuple(i->(isodd(i) ? true : i % UInt8), 228),
+    ]
+    B1 = sizeof(tup)
+    B2 = Base.packedsize(typeof(tup))
+    T2 = eval(Symbol("Int$(B2*8)"))
+    g[B2, B1] = @benchmarkable bench_reinterpret($T2, $(tup))
+end
+
+g = addgroup!(SUITE, "padded_to_padded")
+
+for (tup, T2) in [
+        # Same padding, different positions:
+        (0x01, 1, 0x02) => Tuple{Int32, Int16, Int32},
+        (0x01, 1, 2, ntuple(i->0x01, 100),) => Tuple{UInt64, UInt8, Int64, NTuple{100,Int8}},
+        # small padding to big
+        (1, 0x0001, (0x01, 2, 0x01), 0x01, 1.0) =>
+            Tuple{Int16, Int8, Int64, Int8, Int64, Int64, Int8},
+        # inverse: big padding to small
+        (0x0000, 0x01, 0x0000000000000002, 0x03, 0x0000000000000004, 0x0000000000000005, 0x06) =>
+            Tuple{Int64, UInt16, Tuple{UInt8, Int64, UInt8}, UInt8, Float64},
+        # exactly the same padding positions, just different types:
+        (0x01, 1, 0x02) => Tuple{Int8, Float64, Int8},
+    ]
+    p = Base.packedsize(typeof(tup))
+    B1 = sizeof(tup)
+    B2 = sizeof(T2)
+    g[p, B2, B1] = @benchmarkable bench_reinterpret($T2, $(tup))
+end
 
 end # module
